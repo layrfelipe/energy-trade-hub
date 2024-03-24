b881faff28a55f787e0a66246a17aa4b8758e73a
Layr <layrfpf@gmail.com>
2024-03-24 00:50:27 -0300

    fixing debug page and a few adjustments

--- packages/nextjs/app/profile/page.tsx
+++ packages/nextjs/app/profile/page.tsx
@@ -1,9 +1,27 @@
 "use client";
 
 import type { NextPage } from "next";
+import { useState } from "react";
 import styles from "~~/styles/Profile.module.scss"
 
+
 const Profile: NextPage = () => {
+    const [selectedTab, setSelectedTab] = useState("contratos")
+    
+    const handleSelectTab = (tabIndex: number) => {
+        switch (tabIndex) {
+            case 0:
+                if (selectedTab != "Contratos") setSelectedTab("Contratos")
+                break
+            case 1:
+                if (selectedTab != "Transações") setSelectedTab("Transações")
+                break
+            case 2:
+                if (selectedTab != "Fundos retidos") setSelectedTab("Fundos retidos")
+                break
+        }
+    }
+    
   return (
     <div className={styles.profileContainer}>
         <div className={styles.profileMainDataWrapper}>
@@ -12,7 +30,7 @@ const Profile: NextPage = () => {
             </div>
             <h2>COPEL</h2>
             <div className={styles.tabsWrapper}>
-                <h3>Contratos</h3>
+                <h3 onClick={() => handleSelectTab(0)}>Contratos</h3>
                 <h3>Transações</h3>
                 <h3>Fundos retidos</h3>
             </div>
